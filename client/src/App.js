f392042545a3e1f00aba6e132b08b871e4a8e417
mantrishiva <37258769+mantrishiva@users.noreply.github.com>
2021-10-04 20:20:35 +0530

    added material ui core

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import MessageingApp from  './components/messageingApp/messagingApp';
 
 function App() {
   return (
-    <div className="App">
-      <h1>one to one chat application</h1>
-    </div>
+    <>
+      <MessageingApp />
+    </>
   );
 }
 
